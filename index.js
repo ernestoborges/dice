b164c88742ef4c50967a03cbd70079660c0e9dbc
ErnestoBorges <50407385+ernestoborges@users.noreply.github.com>
2022-05-28 00:15:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,30 +11,30 @@ function alavanca(){
     P2 = rollDice();
 
     if(P1>P2){
-        document.querySelector(".dicebox-lampP1").style.backgroundImage = "url('../v2/images/lamp-on.png')";
-        document.querySelector(".dicebox-lampP2").style.backgroundImage = "url('../v2/images/lamp-off.png')";
-        document.querySelector(".dicebox-label").style.backgroundImage = "url('../v2/images/label1.png')";
+        document.querySelector(".dicebox-lampP1").style.backgroundImage = "url('../images/lamp-on.png')";
+        document.querySelector(".dicebox-lampP2").style.backgroundImage = "url('../images/lamp-off.png')";
+        document.querySelector(".dicebox-label").style.backgroundImage = "url('../images/label1.png')";
     }
     if(P2>P1){
-        document.querySelector(".dicebox-lampP2").style.backgroundImage = "url('../v2/images/lamp-on.png')";
-        document.querySelector(".dicebox-lampP1").style.backgroundImage = "url('../v2/images/lamp-off.png')";
-        document.querySelector(".dicebox-label").style.backgroundImage = "url('../v2/images/label2.png')";
+        document.querySelector(".dicebox-lampP2").style.backgroundImage = "url('../images/lamp-on.png')";
+        document.querySelector(".dicebox-lampP1").style.backgroundImage = "url('../images/lamp-off.png')";
+        document.querySelector(".dicebox-label").style.backgroundImage = "url('../images/label2.png')";
     }
     if(P1===P2){
-        document.querySelector(".dicebox-lampP1").style.backgroundImage = "url('../v2/images/lamp-off.png')";
-        document.querySelector(".dicebox-lampP2").style.backgroundImage = "url('../v2/images/lamp-off.png')";
-        document.querySelector(".dicebox-label").style.backgroundImage = "url('../v2/images/label3.png')";
+        document.querySelector(".dicebox-lampP1").style.backgroundImage = "url('../images/lamp-off.png')";
+        document.querySelector(".dicebox-lampP2").style.backgroundImage = "url('../images/lamp-off.png')";
+        document.querySelector(".dicebox-label").style.backgroundImage = "url('../images/label3.png')";
     }
 
-    document.querySelector(".dicebox-dice1").style.backgroundImage = "url('../v2/images/dice-0"+P1+".png')";
-    document.querySelector(".dicebox-dice2").style.backgroundImage = "url('../v2/images/dice-0"+P2+".png')";    
+    document.querySelector(".dicebox-dice1").style.backgroundImage = "url('../images/dice-0"+P1+".png')";
+    document.querySelector(".dicebox-dice2").style.backgroundImage = "url('../images/dice-0"+P2+".png')";    
 
 }
 
 function push(){
-    document.querySelector(".dicebox-btn").style.backgroundImage = "url('../v2/images/alavanca2.png')"; 
+    document.querySelector(".dicebox-btn").style.backgroundImage = "url('../images/alavanca2.png')"; 
 }
 function release(){
-    document.querySelector(".dicebox-btn").style.backgroundImage = "url('../v2/images/alavanca1.png')"; 
+    document.querySelector(".dicebox-btn").style.backgroundImage = "url('../images/alavanca1.png')"; 
 }
 
